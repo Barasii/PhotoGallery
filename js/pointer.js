913c6cd01052c3834e83b4908cb873a37868c15d
Your Name <you@example.com>
2019-04-25 14:15:03 +0300

    hover

--- js/pointer.js
+++ js/pointer.js
@@ -2,11 +2,25 @@ var arrImages = ["img/artist-3480274__340.jpg", "img/clouds-149344__340.png","im
 
 var imagesField = document.createElement('template');
 	for(var i=0; i<arrImages.length; i++){
-		imagesField.innerHTML += `	<div class="gallery__item">            							
-            							 <a href="#" >
+		imagesField.innerHTML += `	<figure class="gallery__item">   
+                                         <a href="#">
 											<img src="${arrImages[i]}" alt="Logo" class="gallery__item_link">
             							 </a> 
-        							</div>  `;
+                                        <div class="hover-up">
+                                            <div class="hover-up_content">
+                                                <div class="content-text"><span>Maybe some text</span></div>
+                                                <div class="content__circle content-item_up"><a href="#" class="hover-link"><i class="fa fa-plus-circle"></i></a></div>
+                                            </div>
+                                        </div>
+            							 <div class="hover-down">
+                                            <div class="hover-down_content">
+                                                <div class="content__circle content-item_down"><a href="#" class="hover-link"><i class="fa fa-shopping-cart"></i></a></div>
+                                                <div class="content__circle content-item_down"><a href="#" class="hover-link"><i class="fa fa-star-half-full"></i></a></div>
+                                                <div class="content__circle content-item_down"><a href="#" class="hover-link"><i class="fa fa-dropbox"></i></a></div>
+                                            </div>
+                                        </div>  
+
+        							</figure>  `;
 	}
 	document.querySelector(".gallery").appendChild(imagesField.content);
 // }
